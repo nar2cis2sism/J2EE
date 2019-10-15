f4833a97492f7adf0ae50d9771bace8412962291
nar2cis2sism <nar2cis2sism@163.com>
2019-10-15 11:12:59 +0800

    放弃H5

--- ServerManager/WebRoot/js/common.js
+++
@@ -1,6 +0,0 @@
-function adjustFrameSize(iframe) {
-	var iframeWin = iframe.contentWindow || iframe.contentDocument.parentWindow;
-	if (iframeWin) {
-		iframe.height = iframeWin.document.documentElement.scrollHeight || iframeWin.document.body.scrollHeight;
-	}
-};
